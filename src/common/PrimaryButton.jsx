88d45dd91cd9a12b0655d79829b846c30420bbdf
Sandeep <elsenothing321@gmail.com>
2024-08-21 12:44:47 +0530

    added classname as aprop in the page

--- src/common/PrimaryButton.jsx
+++ src/common/PrimaryButton.jsx
@@ -1,6 +1,6 @@
-const PrimaryButton = ({ BtnText }) => {
+const PrimaryButton = ({ BtnText, className }) => {
     return (
-        <button className="font-Rubik font-semibold text-base px-6 py-[14px] bg-light_orange rounded-lg text-white">{BtnText}</button>
+        <button className={`font-Rubik font-semibold text-base px-6 py-[14px] bg-light_orange rounded-lg text-white ${className}`}>{BtnText}</button>
     )
 }
 

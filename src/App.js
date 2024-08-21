f0208aaf46670f9167c486ba52e76111e1859d03
Ritik <rvjaat25@gmail.com>
2024-08-21 13:52:18 +0530

    container-added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import Footer from './common/Footer';
-import Header from './common/Header';
 import Home from './components/views/Home';
 
 function App() {
--- tailwind.config.js
+++ tailwind.config.js
@@ -24,9 +24,16 @@ module.exports = {
         "md": "56px",
         "lg": "66px",
       },
-      backgroundImage:{
-        "orange_gradient":"linear-gradient(241.09deg, rgba(255, 76, 36, 0.15) -34.32%, rgba(255, 227, 108, 0.15) 109.3%)"
-      }
+      backgroundImage: {
+        "orange_gradient": "linear-gradient(241.09deg, rgba(255, 76, 36, 0.15) -34.32%, rgba(255, 227, 108, 0.15) 109.3%)"
+      },
+      container: {
+        padding: {
+          DEFAULT: "20px",
+          sm: "24px",
+        },
+        center: true,
+      },
     },
   },
   plugins: [],

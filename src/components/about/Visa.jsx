0aea60ff7ec67aa6ae69085034161048aff41e3e
ajay-bishnoi <AJAY2003BISHNOI@GMAIL.COM>
2024-08-23 12:29:47 +0530

    adjust the file data

--- src/components/about/Visa.jsx
+++ src/components/about/Visa.jsx
@@ -1,22 +1,7 @@
 import React from "react";
-import Act from "./common/Act";
-import { VISA_DATA } from "../../utils/helper";
-import PrimaryParagraph from "../../common/PrimaryParagraph";
 
 const Visa = () => {
-  return (
-    <div className="xl:pb-[120px] lg:pb-20 pb-16">
-      <div className="container xl:max-w-[1188px] mx-auto">
-        <Act sectionData={VISA_DATA} />
-        <div className="mt-20 xl:mt-16 border rounded-lg bg-white border-light_grey md:p-6 p-4">
-          <PrimaryParagraph
-            className="leading-6"
-            text="By basing its knowledge on these official sources, Herbert provides you with accurate, up-to-date, & reliable information on German immigration processes. Our commitment to using these authoritative resources ensures that you receive guidance aligned with current German immigration laws & practices."
-          />
-        </div>
-      </div>
-    </div>
-  );
+  return <div className="">visa</div>;
 };
 
 export default Visa;

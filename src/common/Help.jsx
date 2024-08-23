4efb3d5c66479b1e12e88b6179a966a8a1a2f6df
Sandeep <elsenothing321@gmail.com>
2024-08-23 12:49:42 +0530

    completed layout of herbert page

--- src/common/Help.jsx
+++ src/common/Help.jsx
@@ -38,7 +38,7 @@ const Help = () => {
                 <PrimaryParagraph text="Lorem ipsum dolor sit amet consectetur adipiscing eli mattis sit phasellus mollis sit aliquam sit nullam." className="max-w-[528px] text-center text-white sm:mt-4 mt-3 sm:mb-11 mb-8" />
                 <div className="flex items-center justify-center sm:gap-4 gap-3 sm:flex-nowrap flex-wrap">
                     <PrimaryButton BtnText="Start your conversation" className="!text-light_orange bg-white max-sm:w-full" />
-                    <PrimaryButton BtnText="Learn More" className="border border-white max-sm:w-full" />
+                    <PrimaryButton BtnText="Learn More" className="border !border-white max-sm:w-full" />
                 </div>
             </div>
         </div>
--- src/common/PrimaryButton.jsx
+++ src/common/PrimaryButton.jsx
@@ -1,6 +1,6 @@
 const PrimaryButton = ({ BtnText, className }) => {
     return (
-        <button className={`font-Rubik font-semibold text-base px-6 py-[14px] bg-light_orange rounded-lg text-white ${className}`}>{BtnText}</button>
+        <button className={`font-Rubik font-semibold text-base px-6 py-[14px] bg-light_orange hover:bg-white hover:border-light_orange hover:border border border-transparent hover:text-light_orange ease-in-out duration-300 rounded-lg text-white ${className}`}>{BtnText}</button>
     )
 }
 

6b9708b4920743e566fac0119f09754567e7c2f8
Sandeep <elsenothing321@gmail.com>
2024-08-28 15:34:41 +0530

    corrected the compelete page mistakes

--- src/common/Help.jsx
+++ src/common/Help.jsx
@@ -12,7 +12,7 @@ const Help = () => {
         const aot = gsap.context(() => {
             let tl = gsap.timeline();
             ScrollTrigger.create({
-                trigger: ".faq_animatio",
+                trigger: ".team_animation",
                 start: "top 40%",
                 end: "bottom 80%",
                 animation: tl,
@@ -28,12 +28,12 @@ const Help = () => {
     }, [])
     let path = window.location.pathname;
     return (
-        <div className="px-6 bg-bg_Linear_Gradient faq_animatio overflow-hidden max-w-[1920px] mx-auto">
+        <div className="px-6 bg-bg_Linear_Gradient team_animation overflow-hidden max-w-[1920px] mx-auto">
             <div className={`container max-w-[1180px] ${path === "/sources" ? "mt-0" : "lg:mt-[120px] sm:mt-20 mt-16"} bg-light_orange sm:py-16 py-5 rounded-xl flex items-center justify-center flex-col main_box`}>
                 <PrimaryHeading className="max-w-[632px] text-center main_box">
-                    <span className="text-white font-rubik sm:!text-4xl xl:!text-[48px] xl:leading-[56px]">Need Help in Your Visa? <br className="lg:block hidden" /> Ask Herbert Now!</span>
+                    <span className="text-white font-rubik sm:!text-5xl xl:!text-[48px] xl:leading-[56px]">Need Help in Your Visa? <br className="lg:block hidden" /> Ask Herbert Now!</span>
                 </PrimaryHeading>
-                <PrimaryParagraph text="Lorem ipsum dolor sit amet consectetur adipiscing eli mattis sit phasellus mollis sit aliquam sit nullam." className="max-w-[528px] text-center text-white sm:mt-4 mt-3 sm:mb-11 mb-8 main_box" />
+                <PrimaryParagraph text="Lorem ipsum dolor sit amet consectetur adipiscing eli mattis sit phasellus mollis sit aliquam sit nullam." className="max-w-[528px] text-center text-white sm:mt-4 mt-3 lg:mb-11 mb-8 main_box" />
                 <div className="flex items-center justify-center sm:gap-4 gap-3 sm:flex-nowrap flex-wrap">
                     <div className="main_box max-sm:w-full">
                         <PrimaryButton BtnText="Start your conversation" className="!text-light_orange bg-white max-sm:w-full firstBtn hover:!bg-transparent hover:!text-white hover:!border hover:!border-white" />
